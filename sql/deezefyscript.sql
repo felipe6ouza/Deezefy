d4e9bbef4368782f1542dae294b1ed90977c88a7
Felipe Souza <felipe6ouza@outlook.com>
2021-02-07 17:59:28 -0300

    Remover tabela de Migração.

--- sql/deezefyscript.sql
+++ sql/deezefyscript.sql
@@ -1,13 +1,4 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
-BEGIN
-    CREATE TABLE [__EFMigrationsHistory] (
-        [MigrationId] nvarchar(150) NOT NULL,
-        [ProductVersion] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
-    );
-END;
-GO
-
+﻿
 BEGIN TRANSACTION;
 GO
 
@@ -253,10 +244,6 @@ CREATE INDEX [IX_OuvintePlaylist_PlaylistsNome] ON [OuvintePlaylist] ([Playlists
 GO
 
 CREATE UNIQUE INDEX [IX_Perfis_OuvinteEmail] ON [Perfis] ([OuvinteEmail]) WHERE [OuvinteEmail] IS NOT NULL;
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20210127001010_Initial', N'5.0.2');
 GO
 
 COMMIT;
