4909dddeabc98716edea006e03099a04e9dc01fd
Felipe Souza <felipe6ouza@outlook.com>
2021-02-09 22:55:14 -0300

    Adiciona configuração do AutoMapper.

---
+++ src/Deezefy.Application/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Deezefy.Application.ViewModels;
+using Deezefy.Business.Models;
+
+namespace Deezefy.Application.Configurations
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<AlbumViewModel, Album>().ReverseMap();
+            CreateMap<ArtistaViewModel, Artista>()
+                .IncludeBase<UsuarioViewModel, Usuario>().ReverseMap();
+            CreateMap<GeneroViewModel, Genero>().ReverseMap();
+            CreateMap<LocalViewModel, Local>().ReverseMap();
+            CreateMap<MusicaViewModel, Musica>().ReverseMap();
+            CreateMap<OuvinteViewModel, Ouvinte>()
+                 .IncludeBase<UsuarioViewModel, Usuario>()
+                .ReverseMap();
+            CreateMap<PerfilViewModel, Perfil>().ReverseMap();
+            CreateMap<PlaylistViewModel, Playlist>().ReverseMap();
+            CreateMap<UsuarioViewModel, Usuario>().ReverseMap();
+
+        }
+    }
+}
